837d888a7d48acb2e23e678f9559211c454ef930
Phani Madiraju <phanimkms1@gmail.com>
2023-08-17 12:02:15 +0530

    Session 2 committed

---
+++ src/OperatorsDemo.java
@@ -0,0 +1,14 @@
+public class OperatorsDemo {
+    public static void main(String[] args) {
+     //   +,-,*,/,%-> modulo
+
+//        System.out.println(2+3);
+//        System.out.println(11/2);
+//        double quotient =11/2;
+//        System.out.println(quotient);
+        System.out.print(11%2);  //1
+        System.out.print(11/2);  //5  //15
+
+
+    }
+}
